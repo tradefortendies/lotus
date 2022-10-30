70c5b168500a0c10a8421447e6b137bd877b5b60
Adam Chambers <adammchambers@gmail.com>
2022-10-29 23:59:33 -0400

    Add titleCase method to helpers module

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -15,3 +15,15 @@ export const formatAddress = (address: string): string =>
     address.length - 4,
     address.length
   )}`
+
+export const titleCase = (str: string): string => {
+  const title: string[] = str.toLowerCase().split(' ')
+
+  const formattedTitle: string = title
+    .map((word) => {
+      return word[0].toUpperCase() + word.slice(1)
+    })
+    .join(' ')
+
+  return formattedTitle
+}
