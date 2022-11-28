5d3526efd952d8cb3481623556987a09dd26de9d
Adam Chambers <adammchambers@gmail.com>
2022-11-28 00:50:55 -0500

    Fix image path in helper function

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -29,7 +29,7 @@ export const titleCase = (str: string): string => {
 }
 
 export const cdnAsset = (col: string, add: string, size: 'large' | 'thumb') => {
-  return `https://lotusgang-assets.sfo3.cdn.digitaloceanspaces.com/collections/${col}/${size}/${add}.jpg`
+  return `https://lotusgang-assets.sfo3.cdn.digitaloceanspaces.com/collections-new/${col}/${size}/${add}.jpg`
 }
 
 export const splitNftName = (name: string): string[] => {
