91f3977d0d7fd7f78dc216b063390b7f4753b203
Adam Chambers <adammchambers@gmail.com>
2022-11-28 17:21:30 -0500

    No scrolling on roadmap

--- pages/blueprint.tsx
+++ pages/blueprint.tsx
@@ -5,7 +5,6 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stage } from '@react-three/drei'
 import Meta from '../components/Meta'
 import Header from '../components/Header'
-import Footer from '../components/Footer'
 import BlueprintModel from '../components/BlueprintModel'
 
 const Blueprint: NextPage = () => {
@@ -31,9 +30,9 @@ const Blueprint: NextPage = () => {
           colorChangeAnimation={false}
           iconHoverColorAnimations={false}
         />
-        <div className="w-screen min-h-screen text-neutral-900 bg-lily-blue">
+        <div className="w-screen h-screen overflow-hidden text-neutral-900 bg-lily-blue">
           <div className="relative">
-            <div className="relative w-full bg-white text-lily-black pb-28">
+            <div className="relative w-full h-screen bg-white text-lily-black pb-28">
               <div className="relative flex flex-col w-full px-4 pt-32 mx-auto lg:px-8 lg:pt-48 max-w-7xl">
                 <div id="masthead" className="text-center">
                   <h1 className="w-full font-mono text-6xl opacity-0 lg:text-7xl">
@@ -74,7 +73,6 @@ const Blueprint: NextPage = () => {
                 </div>
               </div>
             </div>
-            <Footer />
           </div>
         </div>
       </>
