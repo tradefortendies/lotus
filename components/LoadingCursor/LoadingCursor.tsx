57cedc58be2df0593ebe2da0e61da32e3c9c8e7d
Adam Chambers <adammchambers@gmail.com>
2022-11-29 22:42:57 -0500

    Hook up new loading cursor on home page

--- components/LoadingCursor/LoadingCursor.tsx
+++ components/LoadingCursor/LoadingCursor.tsx
@@ -6,6 +6,7 @@ import { ThemeContext } from '../Theme'
 
 function LoadingCursor() {
   const router = useRouter()
+  const loadingRefContainer = useRef<HTMLDivElement>(null)
   const loadingRef = useRef<HTMLDivElement>(null)
   const theme = useContext(ThemeContext)
   const [show, setShow] = useState(false)
@@ -13,14 +14,12 @@ function LoadingCursor() {
 
   useEffect(() => {
     router.events.on('routeChangeStart', () => {
-      console.log('route started')
       if (!loadingRef.current) {
         return
       }
       setShow(true)
     })
     router.events.on('routeChangeComplete', () => {
-      console.log('route complete')
       setTimeout(() => setShow(false), 1000)
     })
     router.events.on('routeChangeError', () => setShow(false))
@@ -36,40 +35,48 @@ function LoadingCursor() {
 
   useEffect(() => {
     document.addEventListener('mousemove', (e) => {
-      console.log([e.clientX, e.clientY])
       setMouseCoords([e.clientX, e.clientY])
 
-      if (!loadingRef.current || !show) {
+      if (!loadingRef.current || !loadingRefContainer.current || !show) {
         return
       }
 
-      loadingRef.current.classList.add('opacity-100')
+      loadingRefContainer.current.classList.add('opacity-100')
       loadingRef.current.style.left = e.clientX + 'px'
       loadingRef.current.style.top = e.clientY + 'px'
     })
 
     document.addEventListener('mouseleave', () => {
-      if (!loadingRef.current) {
+      if (!loadingRefContainer.current) {
         return
       }
 
-      loadingRef.current.classList.remove('opacity-100')
+      loadingRefContainer.current.classList.remove('opacity-100')
     })
   }, [])
 
   return (
-    <div
-      ref={loadingRef}
-      className={clsx(
-        'py-1 px-0 text-xs top-[500px] text-neutral-800 uppercase fixed opacity-0 z-[9999999] transition pointer-events-none',
-        show && 'opacity-100'
-      )}
-      style={{ backgroundColor: theme.primaryColor }}
-    >
-      <Marquee gradient={false} speed={120}>
-        Loading
-      </Marquee>
-    </div>
+    <>
+      <div
+        ref={loadingRefContainer}
+        id="loading-cursor"
+        className={clsx('opacity-0', show && 'opacity-100')}
+      >
+        {mouseCoords[0] > 0 && mouseCoords[1] > 0 && (
+          <div
+            ref={loadingRef}
+            className={clsx(
+              'py-1 px-0 text-xs top-[500px] text-neutral-800 uppercase fixed z-[9999999] transition pointer-events-none'
+            )}
+            style={{ backgroundColor: theme.primaryColor }}
+          >
+            <Marquee gradient={false} speed={120}>
+              Loading
+            </Marquee>
+          </div>
+        )}
+      </div>
+    </>
   )
 }
 
