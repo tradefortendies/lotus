3e30728e79fed1184f94029cdbee808a4deff515
Adam Chambers <adammchambers@gmail.com>
2023-02-10 16:07:21 -0500

    chore: theme color on collection panel UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
-import { useEffect, useRef, useState } from 'react'
-import { gsap } from 'gsap'
+import { useEffect, useRef, useState, useContext } from 'react'
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
+import { ThemeContext } from '../components/Theme'
 import Meta from '../components/Meta'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
@@ -9,6 +9,7 @@ import Panel from '../components/Panel'
 import Button from '../components/Button'
 
 const Home: NextPage = () => {
+  const theme = useContext(ThemeContext)
   const bgRef = useRef<HTMLDivElement>(null)
   const [isLoading, setIsLoading] = useState(false)
   const [windowDimensions, setWindowDimensions] = useState({
@@ -80,7 +81,7 @@ const Home: NextPage = () => {
       <>
         <div
           ref={bgRef}
-          className="fixed flex flex-col-reverse 0 transition-opacity duration-1000 z-[9999] w-screen h-screen bg-neutral-800"
+          className="fixed flex flex-col-reverse 0 transition-opacity duration-1000 z-[9999] pointer-events-none w-screen h-screen bg-neutral-800"
         >
           <div className="w-screen h-1/5 bg-lily-green"></div>
           <div className="w-screen h-1/5 bg-lily-yellow"></div>
@@ -88,10 +89,6 @@ const Home: NextPage = () => {
           <div className="w-screen h-1/5 bg-lily-blue-dark"></div>
           <div className="w-screen h-1/5 bg-lily-blue"></div>
         </div>
-        {/* <div
-          id="intro2"
-          className="absolute z-[9999] top-0 translate-y-[100vh] left-0 w-screen h-screen transition duration-500 bg-blue-400 rounded-b-3xl"
-        ></div> */}
         <Header
           position="slide"
           fadeInAnimation={true}
@@ -124,25 +121,100 @@ const Home: NextPage = () => {
                 <div className="w-[40%] space-y-8">
                   <h3 className="text-5xl font-bold">LILY</h3>
                   <div className="grid grid-cols-2 gap-2 font-mono">
-                    <div className="p-2 text-white rounded-md bg-lily-red">
+                    <div
+                      className="p-2 text-white rounded-md"
+                      style={{ backgroundColor: theme.primaryColor }}
+                    >
                       <dl className="flex justify-between w-full">
                         <dt className="font-bold uppercase">Floor</dt>
                         <dd>6.2</dd>
                       </dl>
                     </div>
-                    <div className="p-2 text-white rounded-md bg-lily-red">
+                    <div
+                      className="p-2 text-white rounded-md"
+                      style={{ backgroundColor: theme.primaryColor }}
+                    >
                       <dl className="flex justify-between w-full">
                         <dt className="font-bold uppercase">Supply</dt>
                         <dd>10,000</dd>
                       </dl>
                     </div>
-                    <div className="p-2 text-white rounded-md bg-lily-red">
+                    <div
+                      className="p-2 text-white rounded-md"
+                      style={{ backgroundColor: theme.primaryColor }}
+                    >
                       <dl className="flex justify-between w-full">
                         <dt className="font-bold uppercase">Vol</dt>
                         <dd>6.2</dd>
                       </dl>
                     </div>
-                    <div className="p-2 text-white rounded-md bg-lily-red">
+                    <div
+                      className="p-2 text-white rounded-md"
+                      style={{ backgroundColor: theme.primaryColor }}
+                    >
+                      <dl className="flex justify-between w-full">
+                        <dt className="font-bold uppercase">Listed</dt>
+                        <dd>6.2</dd>
+                      </dl>
+                    </div>
+                  </div>
+
+                  <div className="grid grid-cols-3 gap-6">
+                    <img
+                      className="rounded-lg"
+                      src="https://lotusgang-assets.sfo3.cdn.digitaloceanspaces.com/collections%2Flily%2Fwebp%2F126FuD1jgFTb8GCvJgMQsUDn2Uh7Bd7eDtPxsBXLsMeo.webp"
+                    />
+                    <img
+                      className="rounded-lg"
+                      src="https://lotusgang-assets.sfo3.cdn.digitaloceanspaces.com/collections%2Flily%2Fwebp%2F12f3x8N9f7zxnrabfXHUGt4XRYUvhNpRBR5jY9VKQ4NH.webp"
+                    />
+                    <img
+                      className="rounded-lg"
+                      src="https://lotusgang-assets.sfo3.cdn.digitaloceanspaces.com/collections%2Flily%2Fwebp%2F12UGYyfPcrQPsJPSsfPnGtdkevsBa4NaHh3VPnp86M7h.webp"
+                    />
+                  </div>
+                  <Button
+                    href="/collections/lily"
+                    type="pill-outline"
+                    className="!border-black !text-black"
+                  >
+                    Explore
+                  </Button>
+                </div>
+                <div className="w-[40%] space-y-8">
+                  <h3 className="text-5xl font-bold">Lotus Gang</h3>
+                  <div className="grid grid-cols-2 gap-2 font-mono">
+                    <div
+                      className="p-2 text-white rounded-md"
+                      style={{ backgroundColor: theme.primaryColor }}
+                    >
+                      <dl className="flex justify-between w-full">
+                        <dt className="font-bold uppercase">Floor</dt>
+                        <dd>6.2</dd>
+                      </dl>
+                    </div>
+                    <div
+                      className="p-2 text-white rounded-md"
+                      style={{ backgroundColor: theme.primaryColor }}
+                    >
+                      <dl className="flex justify-between w-full">
+                        <dt className="font-bold uppercase">Supply</dt>
+                        <dd>4,000</dd>
+                      </dl>
+                    </div>
+                    <div
+                      className="p-2 text-white rounded-md"
+                      style={{ backgroundColor: theme.primaryColor }}
+                    >
+                      <dl className="flex justify-between w-full">
+                        <dt className="font-bold uppercase">Vol</dt>
+                        <dd>6.2</dd>
+                      </dl>
+                    </div>
+                    <div
+                      className="p-2 text-white rounded-md"
+                      style={{ backgroundColor: theme.primaryColor }}
+                    >
                       <dl className="flex justify-between w-full">
                         <dt className="font-bold uppercase">Listed</dt>
                         <dd>6.2</dd>
@@ -163,56 +235,24 @@ const Home: NextPage = () => {
                       src="https://lotusgang-assets.sfo3.cdn.digitaloceanspaces.com/collections%2Flotus-gang%2Fwebp%2F13QK2paaxsZJCmWGX7wctZ81dsquywTkD62b3T8FpPtP.webp"
                     />
                   </div>
-                  <Button href="/collections/lily">Explore</Button>
-                </div>
-                <div className="w-[40%] space-y-8">
-                  <h3 className="text-5xl font-bold">Lotus Gang</h3>
-                  <div className="grid grid-cols-2 gap-2 font-mono">
-                    <div className="p-2 text-white rounded-md bg-lily-red">
-                      <dl className="flex justify-between w-full">
-                        <dt className="font-bold uppercase">Floor</dt>
-                        <dd>6.2</dd>
-                      </dl>
-                    </div>
-                    <div className="p-2 text-white rounded-md bg-lily-red">
-                      <dl className="flex justify-between w-full">
-                        <dt className="font-bold uppercase">Supply</dt>
-                        <dd>4,000</dd>
-                      </dl>
-                    </div>
-                    <div className="p-2 text-white rounded-md bg-lily-red">
-                      <dl className="flex justify-between w-full">
-                        <dt className="font-bold uppercase">Vol</dt>
-                        <dd>6.2</dd>
-                      </dl>
-                    </div>
-                    <div className="p-2 text-white rounded-md bg-lily-red">
-                      <dl className="flex justify-between w-full">
-                        <dt className="font-bold uppercase">Listed</dt>
-                        <dd>6.2</dd>
-                      </dl>
-                    </div>
-                  </div>
-                  <div className="grid grid-cols-3 gap-6">
-                    <img
-                      className="rounded-lg"
-                      src="https://lotusgang-assets.sfo3.cdn.digitaloceanspaces.com/collections%2Flily%2Fwebp%2F126FuD1jgFTb8GCvJgMQsUDn2Uh7Bd7eDtPxsBXLsMeo.webp"
-                    />
-                    <img
-                      className="rounded-lg"
-                      src="https://lotusgang-assets.sfo3.cdn.digitaloceanspaces.com/collections%2Flily%2Fwebp%2F12f3x8N9f7zxnrabfXHUGt4XRYUvhNpRBR5jY9VKQ4NH.webp"
-                    />
-                    <img
-                      className="rounded-lg"
-                      src="https://lotusgang-assets.sfo3.cdn.digitaloceanspaces.com/collections%2Flily%2Fwebp%2F12UGYyfPcrQPsJPSsfPnGtdkevsBa4NaHh3VPnp86M7h.webp"
-                    />
-                  </div>
-                  <Button href="/collections/lotus-gang">Explore</Button>
+                  <Button
+                    href="/collections/lotus-gang"
+                    type="pill-outline"
+                    className="!border-black !text-black"
+                  >
+                    Explore
+                  </Button>
                 </div>
               </div>
             </div>
           </Panel>
-          <Panel floating={false}>Panel 3</Panel>
+          <Panel floating={false}>
+            <div className="w-full p-4 mx-auto max-w-7xl">
+              <h2 className="mt-16 text-3xl text-center">
+                <strong>The Blueprint</strong> is our version of a road map..
+              </h2>
+            </div>
+          </Panel>
           <Panel floating={true}>Panel 4</Panel>
           <Panel floating={false} last={true}>
             Panel 5
