ea9b1829b86195e984ee8ec1ff9cc62b9a509df3
Adam Chambers <adammchambers@gmail.com>
2023-02-20 14:55:31 -0500

    feat: hero background video

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -4,7 +4,7 @@ import { Panel } from './Panel'
 export const Hero = () => {
   return (
     <Panel floating={false} first={true}>
-      <div className="px-8 md:px-0 md:w-[80vw] h-[98vh] flex items-center mx-auto">
+      <div className="px-8 md:px-0 md:w-[80vw] h-[98vh] flex items-center mx-auto relative z-10">
         <div className="space-y-4 md:w-1/2">
           <Fade duration={500} delay={200} fraction={0}>
             <h2 className="text-3xl">
@@ -17,6 +17,16 @@ export const Hero = () => {
           </Fade>
         </div>
       </div>
+
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute bottom-[20px] right-0 object-cover w-auto h-[80vh] z-0"
+      >
+        <source src="/video/lotus-splash.webm" type="video/webm" />
+        Your browser does not support the video tag.
+      </video>
     </Panel>
   )
 }
