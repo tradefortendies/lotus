6e443f112a9fc33585eab1ec03cc81bd02939f5d
Adam Chambers <adammchambers@gmail.com>
2023-02-20 21:24:43 -0500

    feat: blueprint playlist component

--- components/Home/Blueprint.tsx
+++ components/Home/Blueprint.tsx
@@ -1,24 +1,29 @@
-import { Suspense, useRef } from 'react'
+import { Suspense, useRef, useContext } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stage } from '@react-three/drei'
 import { Fade } from 'react-awesome-reveal'
+import { Audio } from 'react-loader-spinner'
 import { useWindowSize } from '../../hooks/useWindowSize'
+import { ThemeContext } from '../Theme'
 import { Panel } from './Panel'
 import BlueprintModel from '../BlueprintModel'
+import blueprintData from '../../data/bluerprint.json'
+import clsx from 'clsx'
 
 export const Blueprint = () => {
+  const theme = useContext(ThemeContext)
   const orbitControlsRef = useRef(null)
   const windowDimensions = useWindowSize()
 
   return (
-    <Panel floating={false} fixedHeight={true}>
+    <Panel floating={false}>
       <div className="flex flex-col justify-center w-full p-4 mx-auto max-w-screen-lily-container">
         <Fade duration={500} delay={200} fraction={0}>
-          <h2 className="mb-[10vh] text-4xl text-center">
+          <h2 className="mb-24 text-4xl text-center">
             <strong>The Blueprint</strong> is our version of a road map.
           </h2>
         </Fade>
-        <div className="flex items-center justify-between gap-16 h-1/2">
+        <div className="flex items-center justify-between gap-16">
           <div className="w-full h-full">
             <Canvas shadows dpr={[1, 2]} camera={{ fov: 35 }}>
               <Suspense fallback={null}>
@@ -43,7 +48,59 @@ export const Blueprint = () => {
               />
             </Canvas>
           </div>
-          <div className="w-full">Playlist goes here</div>
+          <div className="lg:w-[60%]">
+            <div className="px-4 py-8 text-white bg-lily-black rounded-2xl">
+              <h3 className="text-4xl font-bold">The Blueprint</h3>
+
+              <div className="flex flex-row gap-4 px-4 mt-8 mb-2 text-sm font-semibold border-b text-zinc-500 border-zinc-500">
+                <div className="w-8">#</div>
+                <div className="w-full uppercase">Title</div>
+              </div>
+
+              {blueprintData.tracks.map((item, index) => (
+                <div
+                  className={clsx(
+                    'flex flex-row items-center gap-4 p-2 px-4 text-sm rounded-xl',
+                    item.active && 'bg-zinc-600'
+                  )}
+                  style={{
+                    color: item.active ? theme.primaryColor : '#fff',
+                  }}
+                  key={index}
+                >
+                  <div className="w-8">
+                    {!item.active && index}
+                    {item.active && (
+                      <Audio
+                        height="20"
+                        width="20"
+                        color={theme.primaryColor}
+                        ariaLabel="three-dots-loading"
+                      />
+                    )}
+                  </div>
+                  <div className="w-full">
+                    <h4
+                      className="font-semibold"
+                      style={{
+                        color: item.active ? theme.primaryColor : '#fff',
+                      }}
+                    >
+                      {item.title}
+                    </h4>
+                    <p
+                      className={clsx(
+                        !item.active && 'text-slate-500',
+                        item.active && 'text-white'
+                      )}
+                    >
+                      {item.artist}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </Panel>
