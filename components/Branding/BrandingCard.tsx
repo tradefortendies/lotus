9304923dd7ac8fc126ae283866d3f253d9883a06
BPark44 <baileyparkerb@gmail.com>
2023-03-12 09:16:42 +1100

    Fix: `Branding` alignment and hover states

--- components/Branding/BrandingCard.tsx
+++ components/Branding/BrandingCard.tsx
@@ -12,16 +12,16 @@ export function BrandingCard({
     img,
 }: BrandingCardProps) {
     return (
-        <div className="flex flex-col sm:flex-row md:flex-row lg:flex-row py-8 md:gap-40 xl:gap-72 gap-6">
+        <div className="flex flex-col sm:flex-row md:flex-row lg:flex-row py-8 justify-between	">
             <div className="flex flex-col sm:gap-6 gap-3">
                 <h2 className="font-mono text-2xl">{title}</h2>
                 <div className="flex gap-4">
-                    <button className=" rounded-full bg-transparent border-black text-neutral-800 py-1 px-8 border-2">
+                    <button className=" rounded-full bg-transparent border-black text-neutral-800 py-1 px-8 border-2 hover:bg-lily-black hover:text-white">
                         <a href={svgLink} download>
                             SVG
                         </a>
                     </button>
-                    <button className=" rounded-full bg-transparent  border-black text-neutral-800 py-1 px-8 border-2">
+                    <button className=" rounded-full bg-transparent  border-black text-neutral-800 py-1 px-8 border-2 hover:bg-lily-black hover:text-white">
                         <a href={pngLink} download>
                             PNG
                         </a>
